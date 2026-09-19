a681d9e420875732170d1bb3383a5f3136b51688
Artemiy <63671030+Artemiy111@users.noreply.github.com>
2026-09-20 00:21:54 +0300

    chore: cap commit header at 72 characters

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "header-max-length": [2, "always", 72],
     "subject-case": [2, "never", ["upper-case", "pascal-case", "start-case"]],
     "body-max-line-length": [0],
   },
