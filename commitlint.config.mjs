714ccd21c7e10f6410a6d7e67851e382cdfbb633
Artemiy <63671030+Artemiy111@users.noreply.github.com>
2026-09-19 21:51:32 +0000

    chore: forbid commit bodies (#1)

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,6 +2,7 @@ export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 72],
+    "body-empty": [2, "always"],
     "subject-case": [2, "never", ["upper-case", "pascal-case", "start-case"]],
     "body-max-line-length": [0],
   },
