e5d7a895e64f1ad10d2a22cec6932830032d44bd
Artemiy <63671030+Artemiy111@users.noreply.github.com>
2026-09-20 00:11:20 +0300

    chore: add commitlint with husky git hook

---
+++ commitlint.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [2, "never", ["upper-case", "pascal-case", "start-case"]],
+    "body-max-line-length": [0],
+  },
+};
